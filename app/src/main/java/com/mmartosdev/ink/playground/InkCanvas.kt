c34b40f62ed0e76cf07f1a1e0b1d1241a7636fa3
Manel Martos <mmartos@degirona.info>
2024-10-24 10:57:39 +0200

    Remove unused annotation

--- app/src/main/java/com/mmartosdev/ink/playground/InkCanvas.kt
+++ app/src/main/java/com/mmartosdev/ink/playground/InkCanvas.kt
@@ -1,6 +1,5 @@
 package com.mmartosdev.ink.playground
 
-import android.annotation.SuppressLint
 import android.widget.FrameLayout
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +10,6 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.ink.authoring.InProgressStrokesView
 
 @Composable
-@SuppressLint("ClickableViewAccessibility")
 fun InkCanvas(
     modifier: Modifier = Modifier,
 ) {
